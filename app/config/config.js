2625f051f1a814a9bf0fa469aa860de2dc31d694
Paolo Pragides <paolo.pragides@sjrb.ca>
2018-02-04 09:26:35 -0700

    Adding basic logging and error handlign

---
+++ app/config/config.js
@@ -0,0 +1,13 @@
+"use strict";
+
+var config = {
+    harperdb: {
+        host: "localhost",
+        http_port: 9925,
+        https_port: 31283
+    }
+};
+
+module.exports = {
+    config: config
+};
--- app/index.js
+++ app/index.js
@@ -1,1 +1,15 @@
 "use strict";
+
+// Let's require some node modules
+var config = require('./config/config');
+
+// Kill the process on SIGINT
+process.on('SIGINT', function () {
+  console.log('Got SIGINT.  Shutting down.');
+  process.exit();
+});
+
+//Handle uncaught exceptions, to prevent process from terminating.
+process.addListener("uncaughtException", function (err) {
+    console.log("Uncaught exception: " + err);
+});
